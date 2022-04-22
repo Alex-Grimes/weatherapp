273edf677e5feefb7c7dfeb8f60bc99b09c467a5
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2022-04-21 21:43:23 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from "react";
+import axios from 'axios'
+import { Container } from "@mui/material";
+import './index.css'
+
+
+
 
 function App() {
+  //const apikey = APIKEY;
+  //const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Greenville</p>
+          </div>
+          <div className="temp">
+            <h1>60</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
